f0fb9682c522259656d664e21a38c3949708ad76
Sasha01 <sstamenkovski@hso.com>
2023-08-11 14:22:24 +0200

    First changes.

--- aoc_functions.py
+++ aoc_functions.py
@@ -1,11 +1,12 @@
 import copy
 
 #main datase [town 1, town 2, path name, distance between town 1 and town 2, pheromone level, probability to choose the path]
-mainDataset = [[1,2,1,6,0.01,0],[1,3,2,5,0.01,0],[1,4,3,2,0.01,0],[2,3,4,4,0.01,0],[2,4,5,9,0.01,0],[3,4,6,5,0.01,0]]
+mainDataset = [[1,2,1,7,0.01,0],[1,3,2,5,0.01,0],[1,4,3,2,0.01,0],[2,3,4,4,0.01,0],[2,4,5,9,0.01,0],[3,4,6,5,0.01,0]]
+afterVisit = [[1,2,1,6,0.01,0],[1,3,2,5,0.01,0],[1,4,3,2,0.01,0],[2,3,4,4,0.01,0],[2,4,5,9,0.01,0],[3,4,6,5,0.01,0]]
 
 alpha = 1
 beta = 1
-ants = 4
+ants = 1
 evap = 0.9
 numberOfTowns = 4
 sumProb = 0 #initial sum of all probabilities
@@ -21,23 +22,24 @@ class aco(object):
         self.data = mainDataset
 
     def run(self):
-        for i in range(ants):
-            print("Running Self!")
+        for i in range(numberOfTowns - 1):
+#            print("Running Self!")
             self.move(visited)
-            print("Self finished.\n")
+#            print("Self finished.\n")
+#            print("Paths to go are: ", usedPaths)
     
     def dataset(self, visited, usedPaths): #Removes entries from original dataset by path visited, not to go through same path
         #create copy of the mainDataset
         if visited[-1] == 1: #Marks start from beginning, take the original dataset
             tempMainDataset = copy.deepcopy(mainDataset)
-            print("New dateset is ", tempMainDataset)
+#            print("New dateset is ", tempMainDataset)
         else:
             tempMainDataset = copy.deepcopy(mainDataset)
             for k in tempMainDataset:
                 for j in usedPaths:
                     if k[2] == j:
                         tempMainDataset.remove(k)
-            print("New temp dataset from else is ", tempMainDataset)
+#            print("New temp dataset from else is ", tempMainDataset)
         return tempMainDataset
 
     def sumProbabilities(self, data): #Calculate sum of all probabilities
@@ -51,12 +53,11 @@ class aco(object):
     def calcProbs(self, visited):
         highestProb = []
         set = self.dataset(visited, usedPaths)
-        print("Dataset is", set)
-        print("Visisted[-1] is ", visited[-1])
+#        print("Dataset is", set)
+#        print("Visisted[-1] is ", visited[-1])
         visit = visited[-1]
         for l in set:
             if (l[0] == visit or l[1] == visit):
-                #print("visited = ", visited)
                 ni = 1/l[3] #calculate 1/d #calculate ni
                 pher = l[4] + (0.001*1/l[3]) #calculate pheromon level
                 totalProb = self.sumProbabilities(set) #Get sum of probabilities
@@ -64,66 +65,44 @@ class aco(object):
                 l[4] = pher
                 l[5] = prob
                 highestProb.append(l) #write all calculated probabilities in array
-        print("Highestprob is", highestProb, ". Used paths is ", usedPaths)
+#        print("Highestprob is", highestProb)
         return highestProb
 
     def move(self, visited):
-        global mainDataset
+        global mainDataset    
         probabilities = self.calcProbs(visited)
         selectHighestProb = max(probabilities, key=lambda x: x[5]) #Get which list has highest calculated probability
-        print("Highest probability is in ", selectHighestProb)
+#        print("Highest probability is in ", selectHighestProb)
         pathtoUpdate = selectHighestProb[2] #Get path that needs to be updated
-        print("Path to update is: ", pathtoUpdate)
+#        print("Path to update is: ", pathtoUpdate)
         for l in mainDataset:
             if l[2] == selectHighestProb[2]:
-                #print("l[2] = ", l[2], "select = ", selectHighestProb[2])
                 l.pop()
                 l.append(selectHighestProb[5])
-        #print("Newmaindataset is ", mainDataset)
-        #Update the list of visited towns (starting point for each iteration)
         if selectHighestProb[0] == visited[-1]:
-            print("The town to go to is", selectHighestProb[1], "by moving through path", pathtoUpdate)
+#            print("The town to go to is", selectHighestProb[1], "by moving through path", pathtoUpdate)
             visited.append(selectHighestProb[1])
         else:
-            print("The town to go to is", selectHighestProb[0], "by moving through path", pathtoUpdate)
+#            print("The town to go to is", selectHighestProb[0], "by moving through path", pathtoUpdate)
             visited.append(selectHighestProb[0])
         usedPaths.append(pathtoUpdate)
+#        print("Visited places are ", visited, ". Length of Visited is ", len(visited))
+        
+        if len(visited) == numberOfTowns:   
+            for i in mainDataset:
+                if i[0] == 1 and i[1] == visited[-1]:
+                    pher = i[4] + (0.001*1/i[3]) #calculate pheromon level
+                    i[4] = pher
+                    i[5] = (i[4])**alpha*(1/i[3])**beta
+#                    print("The town to go to is 1 ", "by moving through path", i[2])
+                    visited.append(i[0])
+                    usedPaths.append(i[2])
+        print("Visites places are ", visited, "with used paths", usedPaths)
         return mainDataset
 
 
-##################################
-#    def choosePath(self, visited, data):
-#        datasettttt = self.dataset(visited, usedPaths)
-#        #print("Dataset isssss", datasettttt)
-#
-#        for l in datasettttt:
-#            if ((l[0] or l[1]) == visited[-1]):
-#                #print("visited = ", visited)
-#                initpher = l[4] #initial quantity of pheromone
-#                ni = 1/l[3] #calculate 1/d #calculate ni
-#                pher = 0.001*1/l[3] #calculate new pheromon level
-#                l[4] = l[4] + pher
-#                totalProb = self.sumProbabilities(data)
-#                prob = ((pher + initpher)**alpha*(ni)**beta)/(totalProb) #calculate new probablity to visit the location
-#                l[5] = prob
-#                highestProb.append(l) #write all calculated probabilities in array
-#                #print("Highestprob is", highestProb)
-#        selectHighestProb = max(highestProb, key=lambda x: x[5]) #Get which list has highest calculated probability
-#        print("Highest probability is in ", selectHighestProb)
-#        pathtoUpdate = selectHighestProb[2] #Get path that needs to be updated
-#        print("Path to update is: ", pathtoUpdate)
-#        #Update the list of visited towns (starting point for each iteration)
-#        if selectHighestProb[0] == visited[-1]:
-#            print("The town to go to is", selectHighestProb[1], "by moving thrugh path", pathtoUpdate)
-#            visited.append(selectHighestProb[1])
-#            usedPaths.append(pathtoUpdate)
-#        else:
-#            print("The town to go to is", selectHighestProb[0], "by moving thrugh path", pathtoUpdate)
-#            visited.append(selectHighestProb[0])
-#            usedPaths.append(pathtoUpdate)
-
-for i in range(2):
+for i in range(1):
     antcolony = aco(mainDataset)
     paths = antcolony.run()
     i = i + 1
-    print("RUNNING")
+    print("END!")
